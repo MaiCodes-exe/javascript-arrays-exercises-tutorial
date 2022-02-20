fe9f978789219aee748994443653d58ac23c048f
MaiCodes-exe <maise.b.costa@gmail.com>
2022-02-20 16:13:29 +0000

    progress

--- .learn/resets/12-Delete-element/app.js
+++ .learn/resets/12-Delete-element/app.js
@@ -1,10 +1,7 @@
 var people = ['juan','ana','michelle','daniella','stefany','lucy','barak', 'emilio'];
 
 //your code below
-var deletePerson(people){
 
-}
 console.log(deletePerson('daniella'));
 console.log(deletePerson('juan'));
 console.log(deletePerson('emilio'));
-
